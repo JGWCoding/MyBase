662b8c6d600a432d8f22f72f9b7223fd0bb78795
jiangliwen <616912268@qq.com>
2021-12-12 14:57:44 +0800

    BaseActivity 增加一些常用方法回调

--- app/src/main/java/com/_basebase/base/ui/page/BaseActivity.java
+++ app/src/main/java/com/_basebase/base/ui/page/BaseActivity.java
@@ -1,11 +1,17 @@
 package com._basebase.base.ui.page;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.NetworkUtils;
+import com.blankj.utilcode.util.ScreenUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -18,13 +24,50 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     private static boolean isMiUi = false;
 
+    NetworkUtils.OnNetworkStatusChangedListener mOnNetworkStatusChangedListener = new NetworkUtils.OnNetworkStatusChangedListener() {
+        @Override
+        public void onDisconnected() {
+            onNetworkChanged(false);
+        }
+
+        @Override
+        public void onConnected(NetworkUtils.NetworkType networkType) {
+            onNetworkChanged(true);
+        }
+    };
+
+    /**
+     * 网络联网状态改变监听,需注意监听的生命周期,只要页面执行onstop就不监听了
+     *
+     * @param isNetConnect
+     */
+    protected void onNetworkChanged(boolean isNetConnect) {
+        LogUtils.d("网络连接状态改变:当前联网状态" + isNetConnect);
+    }
+
+    /**
+     * 在setContentView之前,实现对window的操作,因少数数类需要,不强制实现
+     */
     protected void initWindow() {
+
+    }
+
+    /**
+     * 是否需要解析intent传过来的数据
+     *
+     * @param intent
+     */
+    protected void parseIntent(Intent intent) {
 
     }
 
     protected abstract int getLayoutId();
 
     protected abstract void initView();
+
+    protected abstract void addListener();
+
+    protected abstract void initOberver();
 
     protected abstract void initData();
 
@@ -33,8 +76,65 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         initWindow();
         setContentView(getLayoutId());
-        initView();
-        initData();
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+        if (getIntent() != null) {
+            parseIntent(getIntent());
+        }
+        initView();     //初始化view信息
+        addListener();  //添加监听
+        initOberver();  //添加ViewModel 数据监听
+        initData();     //初始化数据
+    }
+
+
+    /**
+     * 打印启动activity的信息,方便定位所在的页面
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        NetworkUtils.registerNetworkStatusChangedListener(mOnNetworkStatusChangedListener);
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        NetworkUtils.unregisterNetworkStatusChangedListener(mOnNetworkStatusChangedListener);//页面看不见就不在监听网络变化
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LogUtils.d("current_activity:" + getClass().getSimpleName() + "       具体所在类=" + getClass().getName());
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        onScreenChanged(newConfig);
+    }
+
+    /**
+     * 主要用来监听横竖屏变化,不知道能不能监听现在的分屏屏幕变小
+     *
+     * @param newConfig
+     */
+    protected void onScreenChanged(Configuration newConfig) {
+        LogUtils.v("是否竖屏:" + ScreenUtils.isPortrait());
     }
 
     /**
@@ -60,7 +160,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     /*
      * 静态域，获取系统版本是否基于MIUI
      */
-
     static {
         try {
             @SuppressLint("PrivateApi") Class<?> sysClass = Class.forName("android.os.SystemProperties");
