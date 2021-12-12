6be3b65303a9865aa306f56cadfd3f868b4dc1a0
jiangliwen <616912268@qq.com>
2021-12-12 16:30:28 +0800

    BaseApplication.kt 和 自定义填充顶部状态栏和底部导航栏View

--- app/src/main/java/com/example/zw_engineering/activity/ActivityA.java
+++ app/src/main/java/com/example/zw_engineering/activity/ActivityA.java
@@ -14,7 +14,7 @@ public class ActivityA extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity);
+        setContentView(R.layout.activity_dome);
 
 //        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
 //            @Override
