c4ef53acfac85aca21196df4280e65196f2214ff
jiangliwen <616912268@qq.com>
2021-12-26 19:49:11 +0800

    自定义填充顶部状态栏和底部导航栏

---
+++ app/src/main/java/com/example/zw_engineering/activity/FillStatusBarAndNavActivityDome.java
@@ -0,0 +1,16 @@
+package com.example.zw_engineering.activity;
+
+import android.os.Bundle;
+
+import com.example.zw_engineering.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FillStatusBarAndNavActivityDome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dome_fill_status_bar_and_nav);
+    }
+}
